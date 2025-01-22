ed9053e4c30623ddd8e54782fd6173a92742a7cc
Kevin Buchholz <buchholz.kevin@gmail.com>
2025-01-22 13:19:27 -0500

    Initial Commit

---
+++ HapticHelper/HapticHelperApp.swift
@@ -0,0 +1,17 @@
+//
+//  HapticHelperApp.swift
+//  HapticHelper
+//
+//  Created by Kevin Buchholz on 1/22/25.
+//
+
+import SwiftUI
+
+@main
+struct HapticHelperApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
