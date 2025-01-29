cfa09d6c176f1cbba3531b9c076cd7b92abfc51d
Kevin Buchholz <buchholz.kevin@gmail.com>
2025-01-29 11:47:49 -0500

    Added Off The Shelf Haptics View called Sensory Feedback

---
+++ HapticHelper/SensoryFeedbackView.swift
@@ -0,0 +1,18 @@
+//
+//  SensoryFeedbackView.swift
+//  HapticHelper
+//
+//  Created by Kevin Buchholz on 1/24/25.
+//
+
+import SwiftUI
+
+struct SensoryFeedbackView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    SensoryFeedbackView()
+}
